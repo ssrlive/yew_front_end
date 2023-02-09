64e5b1a0ab6f061b0626ece279acb68bd2948489
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-09 22:19:45 +0800

    hello

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,12 @@ pub fn app() -> Html {
     log!(format!("Hello, {name}!"));
     log!( serde_json::to_string_pretty(&obj).unwrap());
 
+    let class = "my_title";
+
     html! {
-        <h1>{ format!("Hello, {}!", name) }</h1>
+        <>
+        <h1 class= { class } >{ format!("Hello, {}!", name) }</h1>
+        <p>{ "This is a paragraph" }</p>
+        </>
     }
 }
