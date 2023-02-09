864b6c50ba72130d92e125caa6afe518803cfbe5
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-09 22:51:35 +0800

    loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use serde::{Deserialize, Serialize};
 use gloo::console::log;
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -16,14 +16,23 @@ pub fn app() -> Html {
     };
 
     log!(format!("Hello, {name}!"));
-    log!( serde_json::to_string_pretty(&obj).unwrap());
+    log!(serde_json::to_string_pretty(&obj).unwrap());
 
     let class = "my_title";
 
+    let tasks = vec!["task1", "task2", "task3"];
+
     html! {
         <>
-        <h1 class= { class } >{ format!("Hello, {}!", name) }</h1>
-        <p>{ "This is a paragraph" }</p>
+            <h1 class= { class } >{ format!("Hello, {}!", name) }</h1>
+            <p>{ "This is a paragraph" }</p>
+
+            <ul>
+                { for tasks.iter().map(|task| html! { <li>{ task }</li> }) }
+                // or
+                // { tasks.iter().map(|task| html! { <li>{ task }</li> }).collect::<Html>() }
+            </ul>
+
         </>
     }
 }
