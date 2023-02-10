f0e9647f5bcd723f14a363aa850f6a1ea7e9a557
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-11 00:43:54 +0800

    Simplifying Complicated State Updates

--- src/components/molecules/custom_form.rs
+++ src/components/molecules/custom_form.rs
@@ -20,9 +20,10 @@ pub fn custom_form(props: &Props) -> Html {
     let cloned_state = state.clone();
     let on_change0 = props.on_change.clone();
     let on_change = Callback::from(move |value: String| {
-        let mut data = cloned_state.deref().clone();
-        data.username = value.clone();
-        cloned_state.set(data);
+        cloned_state.set(Data {
+            username: value.clone(),
+            ..cloned_state.deref().clone()
+        });
         if let Some(on_change) = &on_change0 {
             on_change.emit(value);
         }
