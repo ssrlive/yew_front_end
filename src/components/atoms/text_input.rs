2a9912dc2ba0c2adaf7eb808f1461cb3a8d976b7
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-10 22:03:02 +0800

    Handling Text-Field Events

--- src/components/atoms/text_input.rs
+++ src/components/atoms/text_input.rs
@@ -1,3 +1,6 @@
+use gloo::console::log;
+use wasm_bindgen::JsCast;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[derive(Properties, Clone, PartialEq)]
@@ -7,7 +10,15 @@ pub struct Props {
 
 #[function_component(TextInput)]
 pub fn text_input(props: &Props) -> Html {
+    let on_change = Callback::from(|event: Event| {
+        let value = event
+            .target()
+            .unwrap()
+            .unchecked_into::<HtmlInputElement>()
+            .value();
+        log!(value);
+    });
     html! {
-        <input type="text" name={ props.name.clone() }/>
+        <input type="text" name={ props.name.clone()} onchange={ on_change }/>
     }
 }
