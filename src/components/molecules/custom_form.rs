c8e967031b3f94bb7f73635d693eecc91c5c193a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-10 23:25:56 +0800

    Use State

--- src/components/molecules/custom_form.rs
+++ src/components/molecules/custom_form.rs
@@ -8,10 +8,21 @@ pub struct Props {
 
 #[function_component(CustomForm)]
 pub fn custom_form(props: &Props) -> Html {
+    let username_state = use_state(String::new);
+    let state = username_state.clone();
+    let on_change0 = props.on_change.clone();
+    let on_change = Callback::from(move |value: String| {
+        state.set(value.clone());
+        if let Some(on_change) = &on_change0 {
+            on_change.emit(value);
+        }
+    });
+
     html! {
         <form>
-            <TextInput name="username" on_change={ props.on_change.clone() }/>
+            <TextInput name="username" on_change={ on_change }/>
             <CustomButton label="Submit"/>
+            <p>{ "Username: " }{ &*username_state }</p>
         </form>
     }
 }
