e9484b315859dd0de8cf029a9a390424ece04a6c
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-14 17:33:12 +0800

    Programatic Linking to Pages

--- src/components/pages/about.rs
+++ src/components/pages/about.rs
@@ -1,12 +1,21 @@
 use crate::router::MyRoute;
 use yew::prelude::*;
-use yew_router::prelude::Link;
+use yew_router::prelude::{use_navigator, Link};
 
 #[function_component(About)]
 pub fn about() -> Html {
+    let navigator = use_navigator().unwrap();
+
+    let onclick_cb = Callback::from(move |_| {
+        navigator.push(&MyRoute::Home);
+    });
     html! {
         <div>
             <h1>{ "About" }</h1>
+            <div>
+                <button onclick = { onclick_cb } >{ "Go home" }</button>
+            </div>
+            <br />
             <div>
                 <Link<MyRoute> to={ MyRoute::Home } >{ "Home" }</Link<MyRoute>>
             </div>
