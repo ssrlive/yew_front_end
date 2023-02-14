cc60e0d42ac97db5bdffacdd2a7f73b65e44d93a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-14 17:14:03 +0800

    Linking to Pages

--- src/components/pages/about.rs
+++ src/components/pages/about.rs
@@ -1,10 +1,15 @@
+use crate::router::MyRoute;
 use yew::prelude::*;
+use yew_router::prelude::Link;
 
 #[function_component(About)]
 pub fn about() -> Html {
     html! {
         <div>
             <h1>{ "About" }</h1>
+            <div>
+                <Link<MyRoute> to={ MyRoute::Home } >{ "Home" }</Link<MyRoute>>
+            </div>
         </div>
     }
 }
--- src/components/pages/home.rs
+++ src/components/pages/home.rs
@@ -1,10 +1,15 @@
+use crate::router::MyRoute;
 use yew::{function_component, html, Html};
+use yew_router::prelude::Link;
 
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
         <div>
             <h1>{ "Home" }</h1>
+            <div>
+                <Link<MyRoute> to={ MyRoute::About } >{ "About" }</Link<MyRoute>>
+            </div>
         </div>
     }
 }
